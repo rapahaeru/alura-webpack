1ddeb2d3b7df354904444a7fe2e015de5178d9ff
Raphael Oliveira <rapahaeru@gmail.com>
2019-01-17 15:36:27 -0200

    separates builds, production and dev

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,9 +1,28 @@
 const path = require('path');
 
+const babiliPlugin = require('babili-webpack-plugin');
+let plugins = [];
+
+if (process.env.NODE_ENV == 'production') {
+  plugins.push( new babiliPlugin() );
+}
+
 module.exports = {
   entry: "./app-src/app.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, //indica a condição na qual nosso loader será aplicado. Usamos a expressão regular /\.js$/ para considerar todos os arquivos que terminam com a extensão .js
+        exclude: /node_modules/, // excluímos a pasta node_modules, pois não faz sentido processar os arquivos dela.
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: plugins,
 }
