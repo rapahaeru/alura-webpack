b6b7a5316a2e92e0e358e021ef595d5069396f07
Raphael Oliveira <rapahaeru@gmail.com>
2019-01-20 14:49:06 -0200

    configure css files to load by webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,14 @@
 const path = require('path');
 
 const babiliPlugin = require('babili-webpack-plugin');
+const extractTextPlugin = require('extract-text-webpack-plugin');
+
 let plugins = [];
+
+// exporta os arquivos de estilo para dist/styles.css
+plugins.push(
+  new extractTextPlugin("styles.css")
+);
 
 if (process.env.NODE_ENV == 'production') {
   plugins.push( new babiliPlugin() );
@@ -25,7 +32,10 @@ module.exports = {
       },
       { 
         test: /\.css$/, 
-        loader: 'style-loader!css-loader' 
+        use: extractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
       },
       { 
         test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
