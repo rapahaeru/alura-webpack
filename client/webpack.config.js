2b8241140850f8ef814d479b6f924ce478a6ea33
Raphael Oliveira <rapahaeru@gmail.com>
2019-01-22 16:09:33 -0200

    prepare the app to deploy with update of the api urls

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -42,7 +42,12 @@ plugins.push(
   )
 );
 
+let SERVICE_URL = JSON.stringify('http://localhost:3000');
+
 if (process.env.NODE_ENV == 'production') {
+  
+  SERVICE_URL = JSON.stringify('http://endereco-da-sua-api');
+
   // otimiza o parseamento dos módulos de script
   plugins.push(new webpack.optimize.ModuleConcatenationPlugin());
 
@@ -58,6 +63,10 @@ if (process.env.NODE_ENV == 'production') {
     canPrint: true
  }));
 }
+
+plugins.push(new webpack.DefinePlugin({
+  SERVICE_URL: SERVICE_URL
+}));
 
 module.exports = {
   entry: {
